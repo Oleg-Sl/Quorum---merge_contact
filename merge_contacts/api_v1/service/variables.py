22616c516ab4828ad3f766ab1dcc6a7f391ded62
Oleg <slepcov_oleg@mail.ru>
2022-09-12 20:02:42 +0700

    Реализовано объединение контактов

--- merge_contacts/api_v1/service/variables.py
+++ merge_contacts/api_v1/service/variables.py
@@ -13,10 +13,10 @@ DUPLICATES_FIELDS = {
 # способы объединения полей контактов
 TYPE_MERGE_FIELD = {
     # 'max_length': ['NAME', ],
-    'max_length': ['UF_CRM_1662984998', ],
+    'max_length': ['NAME', ],
     # 'concat_asc_date': [],
-    'concat_asc_date': ['SOURCE_DESCRIPTION', 'UF_CRM_1662984534', 'UF_CRM_1662984573', 'UF_CRM_1662984592', 'UF_CRM_1662984604'],
-    # 'concat_desc_date': ['LAST_NAME',],
+    'concat_asc_date': ['SOURCE_DESCRIPTION', 'UF_CRM_1662986623502', 'UF_CRM_1662986639015', 'UF_CRM_1662986876354', 'UF_CRM_1662984604'],
+    # 'concat_desc_date': ['LAST_NAME', ],
     'concat_desc_date': ['UF_CRM_1662984465', ],
     # 'multi_field': ['EMAIL', 'PHONE', ]
     # 'multi_field': ['EMAIL', 'PHONE', ]

1dbc28ee458c1842ef262237092b07eac5da1191
Oleg <slepcov_oleg@mail.ru>
2022-09-12 18:44:18 +0700

    Реализовано объединение контактов

--- merge_contacts/merge_contacts/urls.py
+++ merge_contacts/merge_contacts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('merge_contacts/admin/', admin.site.urls),
-    path('merge_contacts/api/v1/', include('api_v1.urls', namespace='api_v1')),
+    path('merge_contacts_2/admin/', admin.site.urls),
+    path('merge_contacts_2/api/v1/', include('api_v1.urls', namespace='api_v1')),
 ]
 
